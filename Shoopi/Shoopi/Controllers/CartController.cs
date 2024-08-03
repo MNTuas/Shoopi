eaaff1655509afb3d6b86e600f691fa28d3bc853
tuan <minhtuankf@gmail.com>
2024-08-03 18:27:45 +0700

    fix

---
+++ Shoopi/Shoopi/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoopi.wwwroot
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddToCart(int id, int quantity = 1)
+        {
+            return View();
+        }
+    }
+}
--- Shoopi/Shoopi/Program.cs
+++ Shoopi/Shoopi/Program.cs
@@ -8,6 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ShoopiContext>();
 builder.Services.AddScoped<IProduct,ProductRepository>();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +31,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
