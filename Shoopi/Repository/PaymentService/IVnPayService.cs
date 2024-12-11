9abc1ccd2fe0a86bbfc23769a9e2c7beb1dc4581
tuan <minhtuankf@gmail.com>
2024-12-11 15:59:14 +0700

    add VNPay payment + fix cart controller

---
+++ Shoopi/Repository/PaymentService/IVnPayService.cs
@@ -0,0 +1,16 @@
+﻿using DAO.ViewModels;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.PaymentService
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
